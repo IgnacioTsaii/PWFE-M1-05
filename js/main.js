92f1ce5a532fb7c2ceccc3d92d75e86c10bb13da
IgnacioTsaii <igu99.tsai@gmail.com>
2019-05-15 14:15:49 -0300

    Programción dinamica con JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
 function saludar(){
 	alert("Hola desde JavaScript!")
 }
+function crearNoticia (titulo, autor, bajada){
+	let plantilla = `<article class="row Noticia"> 
+                        <div class="col-12 col-lg-4">
+                          <img src="img/sorteo-libertadores.jpg" class="img-fluid">
+                        </div>
+
+                        <div class="col-12 col-lg-8">
+                          <h3>${titulo}</h3>
+                          <p>por <strong>${autor}</strong></p>
+                          <p>${bajada}</p>
+                        </div>
+                      </article>`
+    let ficha = document.createElement("div")
+        ficha.classList.add("col-12", "col-md-6", "col-lg-12")
+        ficha.innerHTML = plantilla 
+          
+      document.querySelector("#ultimas-noticias").appendChild(ficha)  
+
+}
+ /* Manipulación de un documento HTML */
+
+ let logo = document.querySelector(".navbar-brand")
+
+ // 3) Manipulación de Comportamiento 
+ logo.onclick = cambiarLogo
+
+ function cambiarLogo(){
+ // 1) Manipulación de Contenido 
+ logo.innerHTML = "Noticias <strong>eXtremas</strong>"
+
+ // 2) Manipulación de Estructura 
+ logo.style.color = "#F00"
+ logo.title = "Todas las noticias"
+ logo.classList.add("text-uppercase")
+}
